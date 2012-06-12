d5dc835fd75e1d1b7462708eb2f65c56861164ae
slauzon <seth.lauzon@gmail.com>
2012-06-12 18:33:24 -0500

    Updated valid extension checking.

--- sabToHandBrake.py
+++ sabToHandBrake.py
@@ -3,8 +3,8 @@ from commands import getoutput
 import os
 import string
 import sys
-import autoProcessTV
 
+valid_extensions = ['.mkv','.avi','.mpg','.ts','.wmv']
 valid_chars = '-_.' + string.ascii_letters + string.digits
 options = '-e x264 -f mp4 -Z "High Profile"'
 
@@ -12,11 +12,11 @@ try:
   path = os.sys.argv[1]
   filename = ''
   for file in os.listdir(path):
-    if (file.find('.mkv') > -1 or file.find('.avi') > -1 or file.find('.mpg') > -1 or file.find('.ts') > -1 or file.find('.wmv')) and file.lower().find('sample') == -1 and file.lower().find('SAMPLE') == -1 and file.find('.mp4') == -1 and file.find('.m4v') == -1:
+    if (os.path.splitext(file)[1] in valid_extensions) and file.lower().find('sample') == -1 and file.lower().find('SAMPLE') == -1 and file.find('.mp4') == -1 and file.find('.m4v') == -1:
       filename = ''.join(c for c in file.replace(' ','.') if c in valid_chars)
-      getoutput('mv "' + path + '/' + file + '" ' + path + '/' + filename)
+      #getoutput('mv "' + path + '/' + file + '" ' + path + '/' + filename)
       cmd= r'cd "' + path + '" && HandBrakeCLI -i ' + filename + ' -o ' + '.'.join(filename.split('.')[:-1]) + '.mp4 ' + options + ' > ' + '.'.join(filename.split('.')[:-1]) + '.log && rm ' + filename
       print(cmd)
-      getoutput(cmd)
+      #getoutput(cmd)
 except Exception, e:
   open(path + '/root/errors.txt','w').write(str(e))
