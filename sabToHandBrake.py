df9861c8728f77e14b62231f171268a06c307bee
slauzon <seth.lauzon@gmail.com>
2012-06-12 22:00:42 -0500

    Code cleanup and reorganization

--- sabToHandBrake.py
+++ sabToHandBrake.py
@@ -8,15 +8,23 @@ valid_extensions = ['.mkv','.avi','.mpg','.ts','.wmv']
 valid_chars = '-_.' + string.ascii_letters + string.digits
 options = '-e x264 -f mp4 -Z "High Profile"'
 
-try:
-  path = os.sys.argv[1]
+def parse(directory):
   filename = ''
-  for file in os.listdir(path):
-    if (os.path.splitext(file)[1] in valid_extensions) and file.lower().find('sample') == -1 and file.lower().find('SAMPLE') == -1 and file.find('.mp4') == -1 and file.find('.m4v') == -1:
-      filename = ''.join(c for c in file.replace(' ','.') if c in valid_chars)
-      #getoutput('mv "' + path + '/' + file + '" ' + path + '/' + filename)
-      cmd= r'cd "' + path + '" && HandBrakeCLI -i ' + filename + ' -o ' + '.'.join(filename.split('.')[:-1]) + '.mp4 ' + options + ' > ' + '.'.join(filename.split('.')[:-1]) + '.log && rm ' + filename
-      print(cmd)
-      #getoutput(cmd)
+  items = os.listdir(directory)
+  for item in items:
+    abspath = os.path.join(directory, item)
+    if (os.path.isdir(abspath)):
+      parse(abspath)
+    else:
+      if (os.path.splitext(item)[1] in valid_extensions) and item.lower().find('sample') == -1 and item.lower().find('SAMPLE') == -1 and item.find('.mp4') == -1 and item.find('.m4v') == -1:
+        filename = ''.join(c for c in item.replace(' ','.') if c in valid_chars)
+        renamecmd = r'mv "' + os.path.join(directory, item) + '" "' + os.path.join(directory, filename) + '"'
+        getoutput(renamecmd)
+        cmd = r'cd "' + directory + '" && HandBrakeCLI -i ' + filename + ' -o ' + '.'.join(filename.split('.')[:-1]) + '.mp4 ' + options + ' > ' + '.'.join(filename.split('.')[:-1]) + '.log && rm ' + filename
+        print(cmd)
+        getoutput(cmd)
+
+try:
+  parse(os.sys.argv[1])
 except Exception, e:
-  open(path + '/root/errors.txt','w').write(str(e))
+  open('/root/errors.txt','w').write(str(e))
